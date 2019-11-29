155b1fd904198989e4e379e27061f353567c7a2f
James Wright <jameswright@jameswright.xyz>
2019-11-29 11:50:38 -0700

    Fix issue with "no changes" aborting
    - Add CNAME functionality
    - Directly push build to GH Pages repo

--- action.sh
+++ action.sh
@@ -29,16 +29,19 @@ curl -sSL https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_
 echo 'Building the hugo site'
 ./hugo
 
-echo 'Cloning the GitHub Pages repo'
-BUILD_DIR=build
-rm -fr "${BUILD_DIR}"
 TARGET_REPO_URL="https://${GITHUB_TOKEN}@github.com/${TARGET_REPO}.git"
-git clone "${TARGET_REPO_URL}" "${BUILD_DIR}"
 
-echo 'Moving the content over'
-cp -r public/* build/
+rm -rf .git
+cd public
+
+if ! [ -z "${CNAME}" ]; then
+    echo '$CNAME set, creating file CNAME'
+    echo ${CNAME} > CNAME
+fi
 
 echo 'Committing the site to git and pushing'
+
+git init
 
 if git config --get user.name; then
     git config --global user.name "${GITHUB_ACTOR}"
@@ -48,17 +51,12 @@ if ! git config --get user.email; then
     git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 fi
 
-cd "${BUILD_DIR}"
-
-if git diff --exit-code; then
-    echo "There is nothing to commit, so aborting"
-    exit 0
-fi
+echo "Getting hash for base repository commit"
+HASH=$(echo $GITHUB_SHA | cut -c1-7)
 
 # Now add all the changes and commit and push
 git add . && \
-git commit -m "Publishing site $(date)" && \
-git push origin master
-
+git commit -m "Auto Publishing Site from ${GITHUB_REPOSITORY}@${HASH}" && \
+git push --force $TARGET_REPO_URL master:master
 
 echo 'Complete'
