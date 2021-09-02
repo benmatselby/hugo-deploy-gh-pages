eb12a0655de73249af742c259aa7e4662fc9e3ca
Ben Selby <benmatselby@gmail.com>
2021-09-02 13:30:29 +0100

    Bump Hugo to 0.88.0

--- action.sh
+++ action.sh
@@ -23,7 +23,7 @@ if [[ -z "${TARGET_BRANCH}" ]]; then
 fi
 
 if [[ -z "${HUGO_VERSION}" ]]; then
-    HUGO_VERSION=0.83.1
+    HUGO_VERSION=0.88.0
     echo "No HUGO_VERSION was set, so defaulting to ${HUGO_VERSION}"
 fi
 
